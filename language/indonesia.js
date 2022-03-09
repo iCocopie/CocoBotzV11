d957d509bcbc2eed0722224db80d13925723023a
Cocopie <92080735+iCocopie@users.noreply.github.com>
2022-03-09 16:27:27 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -15,7 +15,7 @@ exports.erorLink = () => {
     return `⚠️ Link Error`
 }
 exports.media = () => {
-    return `Silahkan Pilih Media Yang Ijgin Kamu Download`
+    return `Silahkan Pilih Media Yang Ingin Kamu Download`
 }
 exports.replyImg = (prefix, command) => {
     return `Kirim/Reply Foto Dengan Caption ${prefix + command}`
@@ -99,473 +99,269 @@ exports.menunya = (salam, pushname, botname) =>{
 }
 exports.listMenu = (time, salam, pushname, prefix) => {
     return `*Selamat ${salam} ${pushname}*
-*⌚Time Server : ${time}*
+*⌚ Time Server : ${time}*
 *📚 List-Menu CocoBotz-MD :*
 
-╭─❒ 「 Game Menu 」
-├ ${prefix}kuismath
-├ ${prefix}tebak gambar
-├ ${prefix}tebak kata
-├ ${prefix}tebak bendera
-├ ${prefix}tebak kalimat
-├ ${prefix}tebak siapa
-├ ${prefix}tebak kabupaten
-├ ${prefix}tebak kimia
-├ ${prefix}tebak lirik
-├ ${prefix}tebak tebakan
-├ ${prefix}tekateki
-├ ${prefix}susunkata
-╰❒ ${prefix}caklontong
+ 「 Game Menu 」
+${prefix}suitpvp
+${prefix}tebak gambar
+${prefix}tebak kata
+${prefix}tebak bendera
+${prefix}tebak kalimat
+${prefix}tebak siapa
+${prefix}tebak kabupaten
+${prefix}tebak kimia
+${prefix}tebak lirik
+${prefix}tebak tebakan
+${prefix}tekateki
+${prefix}susunkata
+${prefix}caklontong
 
 
-╭─❒ 「 Bot Info 」 
-├ ${prefix}owner
-├ ${prefix}rules
-├ ${prefix}sc
-├ ${prefix}ping
-├ ${prefix}runtime
-├ ${prefix}botstatus
-╰❒ ${prefix}donate
+ 「 Halal Menu 」
+${prefix}alquran
+${prefix}juzamma
+${prefix}iqra
+${prefix}hadist
 
 
-╭─❒ 「 Owner 」 
-├ < evaluate
-├ > evaluate
-├ $ exec
-├ => exec
-├ ${prefix}setmenu [query]
-├ ${prefix}setmenu templateLocation
-├ ${prefix}setmenu templateTenor
-├ ${prefix}setmenu katalog
-├ ${prefix}setmenu katalog2
-├ ${prefix}setmenu list
-├ ${prefix}setwm packname|author
-├ ${prefix}sendsesi
-├ ${prefix}listpc
-├ ${prefix}listgc
-├ ${prefix}broadcast [text]
-├ ${prefix}bc [text]
-├ ${prefix}bcgc [text]
-├ ${prefix}nsfw [on/off]
-├ ${prefix}autorespond [on/off]
-├ ${prefix}antiviewonce [on/off]
-├ ${prefix}join [link]
-├ ${prefix}self
-├ ${prefix}public [only bot]
-├ ${prefix}del [reply pesan bot]
-╰❒ ${prefix}setppbot [reply image]
+ 「 Bot Info 」 
+${prefix}owner
+${prefix}rules
+${prefix}sc
+${prefix}ping
+${prefix}runtime
+${prefix}botstatus
+${prefix}donate
 
 
-╭─❒ 「 Database 」 
-├ ${prefix}setcmd [reply stiker]
-├ ${prefix}delcmd [reply stiker]
-├ ${prefix}listcmd
-├ ${prefix}absen
-├ ${prefix}cekabsen
-├ ${prefix}deleteabsen
-├ ${prefix}absenstart
-├ ${prefix}addmsg [nama file]
-├ ${prefix}getmsg [nama file]
-├ ${prefix}listmsg
-╰❒ ${prefix}delmsg [nama file]
+ 「 Owner 」 
+< evaluate
+> evaluate
+$ exec
+=> exec
+${prefix}setmenu [query]
+${prefix}setmenu templateLocation
+${prefix}setmenu templateTenor
+${prefix}setmenu katalog
+${prefix}setmenu katalog2
+${prefix}setmenu list
+${prefix}setwm packname|author
+${prefix}sendsesi
+${prefix}listpc
+${prefix}listgc
+${prefix}broadcast [text]
+${prefix}bc [text]
+${prefix}bcgc [text]
+${prefix}nsfw [on/off]
+${prefix}autorespond [on/off]
+${prefix}antiviewonce [on/off]
+${prefix}join [link]
+${prefix}self
+${prefix}public [only bot]
+${prefix}del [reply pesan bot]
+${prefix}setppbot [reply image]
 
 
-╭─❒ 「 Group 」 
-├ ${prefix}listonline
-├ ${prefix}sider
-├ ${prefix}wm packname|author
-├ ${prefix}infochat
-├ ${prefix}setdesk [text]
-├ ${prefix}setppgrup [reply image]
-├ ${prefix}antilink [on/off]
-├ ${prefix}revoke
-├ ${prefix}leave
-├ ${prefix}add [62***]
-├ ${prefix}kick @tag
-├ ${prefix}leave
-├ ${prefix}linkgc
-├ ${prefix}take packname|author
-├ ${prefix}group [open/close]
-├ ${prefix}tagall [text]
-╰❒ ${prefix}hidetag [text]
+ 「 Database 」 
+${prefix}setcmd [reply stiker]
+${prefix}delcmd [reply stiker]
+${prefix}listcmd
+${prefix}absen
+${prefix}cekabsen
+${prefix}deleteabsen
+${prefix}absenstart
+${prefix}addmsg [nama file]
+${prefix}getmsg [nama file]
+${prefix}listmsg
+${prefix}delmsg [nama file]
 
 
-╭─❒ 「 Anime 」 
-├ ${prefix}quotesanime
-├ ${prefix}anime [query]
-├ ${prefix}manga [query]
-╰❒ ${prefix}character [query]
+ 「 Group 」 
+${prefix}listonline
+${prefix}sider
+${prefix}wm packname|author
+${prefix}infochat
+${prefix}setdesk [text]
+${prefix}setppgrup [reply image]
+${prefix}antilink [on/off]
+${prefix}revoke
+${prefix}leave
+${prefix}add [62***]
+${prefix}kick @tag
+${prefix}leave
+${prefix}linkgc
+${prefix}take packname|author
+${prefix}group [open/close]
+${prefix}tagall [text]
+${prefix}hidetag [text]
 
 
-╭─❒ 「 Tag 」 
-├ ${prefix}stickertag
-├ ${prefix}videotag [query]
-├ ${prefix}vntag [query]
-╰❒ ${prefix}imagetag [query]
+ 「 Anime 」 
+${prefix}quotesanime
+${prefix}neko
+${prefix}waifu
+${prefix}shinobu
+${prefix}megumin
+${prefix}cry
+${prefix}anime [query]
+${prefix}manga [query]
+${prefix}character [query]
 
 
-╭─❒ 「 Stalking 」 
-├ ${prefix}igstalk [username]
-├ ${prefix}ghstalk [username]
-╰❒ ${prefix}ytstalk [channel]
+ 「 Tag 」 
+${prefix}stickertag
+${prefix}videotag [query]
+${prefix}vntag [query]
+${prefix}imagetag [query]
 
 
-╭─❒ 「 Search 」 
-├ ${prefix}ytsearch [query]
-├ ${prefix}wallpaper [query]
-├ ${prefix}wikimedia [query]
-├ ${prefix}hentai
-├ ${prefix}wattpad [query]
-├ ${prefix}webtoons [query]
-├ ${prefix}drakor [query]
-╰❒ ${prefix}pinterest [query]
+ 「 Stalking 」 
+${prefix}igstalk [username]
+${prefix}ghstalk [username]
+${prefix}ytstalk [channel]
 
 
-╭─❒ 「 Converter 」
-├ ${prefix}toaudio [video]
-├ ${prefix}tomp3 [video]
-├ ${prefix}tovn [video]
-├ ${prefix}stiker [reply image]
-├ ${prefix}tourl [image/video/stiker]
-├ ${prefix}togif [sticker]
-├ ${prefix}tomp4 [sticker]
-╰❒${prefix}toimg [reply sticker]
+ 「 Search 」 
+${prefix}ytsearch [query]
+${prefix}wallpaper [query]
+${prefix}wikimedia [query]
+${prefix}hentai
+${prefix}wattpad [query]
+${prefix}webtoons [query]
+${prefix}drakor [query]
+${prefix}pinterest [query]
 
 
-╭─❒ 「 Image Effect 」 
-├ ${prefix}wanted [reply image/stiker]
-├ ${prefix}utatoo [reply image/stiker]
-├ ${prefix}unsharpen [reply image/stiker]
-├ ${prefix}thanos [reply image/stiker]
-├ ${prefix}sniper [reply image/stiker]
-├ ${prefix}sharpen [reply image/stiker]
-├ ${prefix}sepia [reply image/stiker]
-├ ${prefix}scary [reply image/stiker]
-├ ${prefix}rip [reply image/stiker]
-├ ${prefix}redple [reply image/stiker]
-├ ${prefix}rejected [reply image/stiker]
-├ ${prefix}posterize [reply image/stiker]
-├ ${prefix}ps4 [reply image/stiker]
-├ ${prefix}pixelize [reply image/stiker]
-├ ${prefix}missionpassed [reply image/stiker]
-├ ${prefix}moustache [reply image/stiker]
-├ ${prefix}lookwhatkarenhave [reply image/stiker]
-├ ${prefix}jail [reply image/stiker]
-├ ${prefix}invert [reply image/stiker]
-├ ${prefix}instagram [reply image/stiker]
-├ ${prefix}greyscale [reply image/stiker]
-├ ${prefix}glitch [reply image/stiker]
-├ ${prefix}gay [reply image/stiker]
-├ ${prefix}frame [reply image/stiker]
-├ ${prefix}fire [reply image/stiker]
-├ ${prefix}distort [reply image/stiker]
-├ ${prefix}dictator [reply image/stiker]
-├ ${prefix}deepfry [reply image/stiker]
-├ ${prefix}ddungeon [reply image/stiker]
-├ ${prefix}circle [reply image/stiker]
-├ ${prefix}challenger [reply image/stiker]
-├ ${prefix}burn [reply image/stiker]
-├ ${prefix}brazzers [reply image/stiker]
-╰❒ ${prefix}beautiful [reply image/stiker]
+ 「 Converter 」
+${prefix}toaudio [video]
+${prefix}tomp3 [video]
+${prefix}tovn [video]
+${prefix}stiker [reply image]
+${prefix}tourl [image/video/stiker]
+${prefix}togif [sticker]
+${prefix}tomp4 [sticker]
+${prefix}toimg [reply sticker]
 
 
-╭─❒ 「 Sticker Effect 」 
-├ ${prefix}jail [reply image/stiker]
-├ ${prefix}red [reply image/stiker]
-├ ${prefix}gay [reply image/stiker]
-├ ${prefix}bloo [reply image/stiker]
-├ ${prefix}blue [reply image/stiker]
-├ ${prefix}sepia [reply image/stiker]
-├ ${prefix}green [reply image/stiker]
-├ ${prefix}glass [reply image/stiker]
-├ ${prefix}invert [reply image/stiker]
-├ ${prefix}blurple [reply image/stiker]
-├ ${prefix}blurple2 [reply image/stiker]
-├ ${prefix}wasted [reply image/stiker]
-├ ${prefix}passed [reply image/stiker]
-├ ${prefix}triggered [reply image/stiker]
-├ ${prefix}comrade [reply image/stiker]
-├ ${prefix}greyscale [reply image/stiker]
-├ ${prefix}threshold [reply image/stiker]
-├ ${prefix}brightness [reply image/stiker]
-╰❒ ${prefix}invertgreyscale [reply image/stiker]
+ 「 Image Effect 」 
+${prefix}wanted [reply image/stiker]
+${prefix}utatoo [reply image/stiker]
+${prefix}unsharpen [reply image/stiker]
+${prefix}thanos [reply image/stiker]
+${prefix}sniper [reply image/stiker]
+${prefix}sharpen [reply image/stiker]
+${prefix}sepia [reply image/stiker]
+${prefix}scary [reply image/stiker]
+${prefix}rip [reply image/stiker]
+${prefix}redple [reply image/stiker]
+${prefix}rejected [reply image/stiker]
+${prefix}posterize [reply image/stiker]
+${prefix}ps4 [reply image/stiker]
+${prefix}pixelize [reply image/stiker]
+${prefix}missionpassed [reply image/stiker]
+${prefix}moustache [reply image/stiker]
+${prefix}lookwhatkarenhave [reply image/stiker]
+${prefix}jail [reply image/stiker]
+${prefix}invert [reply image/stiker]
+${prefix}instagram [reply image/stiker]
+${prefix}greyscale [reply image/stiker]
+${prefix}glitch [reply image/stiker]
+${prefix}gay [reply image/stiker]
+${prefix}frame [reply image/stiker]
+${prefix}fire [reply image/stiker]
+${prefix}distort [reply image/stiker]
+${prefix}dictator [reply image/stiker]
+${prefix}deepfry [reply image/stiker]
+${prefix}ddungeon [reply image/stiker]
+${prefix}circle [reply image/stiker]
+${prefix}challenger [reply image/stiker]
+${prefix}burn [reply image/stiker]
+${prefix}brazzers [reply image/stiker]
+${prefix}beautiful [reply image/stiker]
 
 
-╭─❒ 「 Download 」 
-├ ${prefix}tiktok [link]
-├ ${prefix}tiktoknowm [link]
-├ ${prefix}tiktokwm [link]
-├ ${prefix}tiktokaudio [link]
-├ ${prefix}ytdl [link]
-├ ${prefix}play [query]
-├ ${prefix}ytmp3 [link]
-├ ${prefix}ytshortmp3 [link]
-├ ${prefix}ytmp4 [link]
-├ ${prefix}ytshorts [link]
-├ ${prefix}facebook [link]
-├ ${prefix}facebooksd [link]
-├ ${prefix}facebookhd [link]
-├ ${prefix}fbaudio [link]
-├ ${prefix}igstory [username]
-├ ${prefix}igdl [link]
-├ ${prefix}igphoto [link]
-├ ${prefix}igvideo [link]
-├ ${prefix}igreels [link]
-├ ${prefix}igtv [link]
-├ ${prefix}soundcloud [link]
-├ ${prefix}gitclone [link repo]
-├ ${prefix}gitrepo [username repo branch]
-├ ${prefix}mediafire [link]
-╰❒ ${prefix}twitter link
-
-╭─❒ 「 Primbon 」
-├ ${prefix}nomorhoki 887435047326
-├ ${prefix}artimimpi [query]
-├ ${prefix}artinama [query]
-├ ${prefix}ramaljodoh
-├ ${prefix}ramaljodohbali
-├ ${prefix}suamiistri
-├ ${prefix}ramalcinta
-├ ${prefix}cocoknama
-├ ${prefix}pasangan
-├ ${prefix}jadiannikah
-├ ${prefix}sifatusaha
-├ ${prefix}rezeki
-├ ${prefix}pekerjaan
-├ ${prefix}nasib
-├ ${prefix}penyakit
-├ ${prefix}tarot
-├ ${prefix}fengshui
-├ ${prefix}haribaik
-├ ${prefix}harisangar
-├ ${prefix}harisial
-├ ${prefix}nagahari
-├ ${prefix}arahrezeki
-├ ${prefix}peruntungan
-├ ${prefix}weton
-├ ${prefix}karakter
-├ ${prefix}keberuntungan
-├ ${prefix}memancing
-├ ${prefix}masasubur
-├ ${prefix}zodiak 
-╰❒ ${prefix}shio [query]
+ 「 Sticker Effect 」 
+${prefix}jail [reply image/stiker]
+${prefix}red [reply image/stiker]
+${prefix}gay [reply image/stiker]
+${prefix}bloo [reply image/stiker]
+${prefix}blue [reply image/stiker]
+${prefix}sepia [reply image/stiker]
+${prefix}green [reply image/stiker]
+${prefix}glass [reply image/stiker]
+${prefix}invert [reply image/stiker]
+${prefix}blurple [reply image/stiker]
+${prefix}blurple2 [reply image/stiker]
+${prefix}wasted [reply image/stiker]
+${prefix}passed [reply image/stiker]
+${prefix}triggered [reply image/stiker]
+${prefix}comrade [reply image/stiker]
+${prefix}greyscale [reply image/stiker]
+${prefix}threshold [reply image/stiker]
+${prefix}brightness [reply image/stiker]
+${prefix}invertgreyscale [reply image/stiker]
 
 
-╭─❒ 「 Random Anime 」
-├ ${prefix}loli
-├ ${prefix}neko
-├ ${prefix}waifu
-├ ${prefix}shinobu
-├ ${prefix}megumin
-├ ${prefix}bully
-├ ${prefix}cuddle
-├ ${prefix}cry
-├ ${prefix}hug
-├ ${prefix}awoo
-├ ${prefix}kiss
-├ ${prefix}lick
-├ ${prefix}pat
-├ ${prefix}smug
-├ ${prefix}bonk
-├ ${prefix}yeet
-├ ${prefix}blush
-├ ${prefix}smile
-├ ${prefix}wave
-├ ${prefix}highfive
-├ ${prefix}handhold
-├ ${prefix}nom
-├ ${prefix}bite
-├ ${prefix}glomp
-├ ${prefix}slap
-├ ${prefix}kill
-├ ${prefix}happy
-├ ${prefix}wink
-├ ${prefix}poke
-├ ${prefix}dance
-╰❒ ${prefix}cringe
+ 「 Download 」 
+${prefix}tiktok [link]
+${prefix}tiktoknowm [link]
+${prefix}tiktokwm [link]
+${prefix}tiktokaudio [link]
+${prefix}ytdl [link]
+${prefix}play [query]
+${prefix}ytmp3 [link]
+${prefix}ytshortmp3 [link]
+${prefix}ytmp4 [link]
+${prefix}ytshorts [link]
+${prefix}facebook [link]
+${prefix}facebooksd [link]
+${prefix}facebookhd [link]
+${prefix}fbaudio [link]
+${prefix}igstory [username]
+${prefix}igdl [link]
+${prefix}igphoto [link]
+${prefix}igvideo [link]
+${prefix}igreels [link]
+${prefix}igtv [link]
+${prefix}soundcloud [link]
+${prefix}gitclone [link repo]
+${prefix}gitrepo [username repo branch]
+${prefix}mediafire [link]
+${prefix}twitter link
 
 
-╭─❒ 「 Textpro Menu 」
-├ ${prefix}halloween2 text|text2
-├ ${prefix}horror text|text2
-├ ${prefix}game8bit text|text2
-├ ${prefix}layered text|text2
-├ ${prefix}glitch2 text|text2
-├ ${prefix}coolg text|text2
-├ ${prefix}coolwg text|text2
-├ ${prefix}realistic text|text2
-├ ${prefix}space3d text|text2
-├ ${prefix}gtiktok text|text2
-├ ${prefix}stone text|text2
-├ ${prefix}marvel text|text2
-├ ${prefix}marvel2 text|text2
-├ ${prefix}pornhub text|text2
-├ ${prefix}avengers text|text2
-├ ${prefix}metalr text|text2
-├ ${prefix}metalg text|text2
-├ ${prefix}metalg2 text|text2
-├ ${prefix}halloween2 text|text2
-├ ${prefix}lion text|text2
-├ ${prefix}wolf_bw text|text2
-├ ${prefix}wolf_g text|text2
-├ ${prefix}ninja text|text2
-├ ${prefix}3dsteel text|text2
-├ ${prefix}horror2 text|text2
-├ ${prefix}lava text|text2
-├ ${prefix}bagel text|text2
-├ ${prefix}blackpink text
-├ ${prefix}rainbow2 text
-├ ${prefix}water_pipe text
-├ ${prefix}halloween text
-├ ${prefix}sketch text
-├ ${prefix}sircuit text
-├ ${prefix}discovery text
-├ ${prefix}metallic2 text
-├ ${prefix}fiction text
-├ ${prefix}demon text
-├ ${prefix}transformer text
-├ ${prefix}berry text
-├ ${prefix}thunder text
-├ ${prefix}magma text
-├ ${prefix}3dstone text
-├ ${prefix}neon text
-├ ${prefix}glitch text
-├ ${prefix}harry_potter text
-├ ${prefix}embossed text
-├ ${prefix}broken text
-├ ${prefix}papercut text
-├ ${prefix}gradient text
-├ ${prefix}glossy text
-├ ${prefix}watercolor text
-├ ${prefix}multicolor text
-├ ${prefix}neon_devil text
-├ ${prefix}underwater text
-├ ${prefix}bear text
-├ ${prefix}wonderfulg text
-├ ${prefix}christmas text
-├ ${prefix}neon_light text
-├ ${prefix}snow text
-├ ${prefix}cloudsky text
-├ ${prefix}luxury2 text
-├ ${prefix}gradient2 text
-├ ${prefix}summer text
-├ ${prefix}writing text
-├ ${prefix}engraved text
-├ ${prefix}summery text
-├ ${prefix}3dglue text
-├ ${prefix}metaldark text
-├ ${prefix}neonlight text
-├ ${prefix}oscar text
-├ ${prefix}minion text
-├ ${prefix}holographic text
-├ ${prefix}purple text
-├ ${prefix}glossyb text
-├ ${prefix}deluxe2 text
-├ ${prefix}glossyc text
-├ ${prefix}fabric text
-├ ${prefix}neonc text
-├ ${prefix}newyear text
-├ ${prefix}newyear2 text
-├ ${prefix}metals text
-├ ${prefix}xmas text
-├ ${prefix}blood text
-├ ${prefix}darkg text
-├ ${prefix}joker text
-├ ${prefix}wicker text
-├ ${prefix}natural text
-├ ${prefix}firework text
-├ ${prefix}skeleton text
-├ ${prefix}balloon text
-├ ${prefix}balloon2 text
-├ ${prefix}balloon3 text
-├ ${prefix}balloon4 text
-├ ${prefix}balloon5 text
-├ ${prefix}balloon6 text
-├ ${prefix}balloon7 text
-├ ${prefix}steel text
-├ ${prefix}gloss text
-├ ${prefix}denim text
-├ ${prefix}decorate text
-├ ${prefix}decorate2 text
-├ ${prefix}peridot text
-├ ${prefix}rock text
-├ ${prefix}glass text
-├ ${prefix}glass2 text
-├ ${prefix}glass3 text
-├ ${prefix}glass4 text
-├ ${prefix}glass5 text
-├ ${prefix}glass6 text
-├ ${prefix}glass7 text
-├ ${prefix}glass8 text
-├ ${prefix}captain_as2 text
-├ ${prefix}robot text
-├ ${prefix}equalizer text
-├ ${prefix}toxic text
-├ ${prefix}sparkling text
-├ ${prefix}sparkling2 text
-├ ${prefix}sparkling3 text
-├ ${prefix}sparkling4 text
-├ ${prefix}sparkling5 text
-├ ${prefix}sparkling6 text
-├ ${prefix}sparkling7 text
-├ ${prefix}decorative text
-├ ${prefix}chocolate text
-├ ${prefix}strawberry text
-├ ${prefix}koifish text
-├ ${prefix}bread text
-├ ${prefix}matrix text
-├ ${prefix}blood2 text
-├ ${prefix}neonligth2 text
-├ ${prefix}thunder2 text
-├ ${prefix}3dbox text
-├ ${prefix}neon2 text
-├ ${prefix}roadw text
-├ ${prefix}bokeh text
-├ ${prefix}gneon text
-├ ${prefix}advanced text
-├ ${prefix}dropwater text
-├ ${prefix}wall text
-├ ${prefix}chrismast text
-├ ${prefix}honey text
-├ ${prefix}drug text
-├ ${prefix}marble text
-├ ${prefix}marble2 text
-├ ${prefix}ice text
-├ ${prefix}juice text
-├ ${prefix}rusty text
-├ ${prefix}abstra text
-├ ${prefix}biscuit text
-├ ${prefix}wood text
-├ ${prefix}scifi text
-├ ${prefix}metalr text
-├ ${prefix}purpleg text
-├ ${prefix}shiny text 
-├ ${prefix}jewelry text
-├ ${prefix}jewelry2 text
-├ ${prefix}jewelry3 text
-├ ${prefix}jewelry4 text
-├ ${prefix}jewelry5 text
-├ ${prefix}jewelry6 text
-├ ${prefix}jewelry7 text
-├ ${prefix}jewelry8 text
-├ ${prefix}metalh text
-├ ${prefix}golden text
-├ ${prefix}glitter text
-├ ${prefix}glitter2 text
-├ ${prefix}glitter3 text
-├ ${prefix}glitter4 text
-├ ${prefix}glitter5 text
-├ ${prefix}glitter6 text
-├ ${prefix}glitter7 text
-├ ${prefix}metale text
-├ ${prefix}carbon text
-├ ${prefix}candy text
-├ ${prefix}metalb text
-├ ${prefix}gemb text
-├ ${prefix}3dchrome text
-├ ${prefix}metalb2 text
-├ ${prefix}metalg text
-╰❒ ${prefix}metalg text
+ 「 Primbon 」
+${prefix}nomorhoki 887435047326
+${prefix}artimimpi [query]
+${prefix}artinama [query]
+${prefix}ramaljodoh
+${prefix}ramaljodohbali
+${prefix}suamiistri
+${prefix}ramalcinta
+${prefix}cocoknama
+${prefix}pasangan
+${prefix}jadiannikah
+${prefix}sifatusaha
+${prefix}rezeki
+${prefix}pekerjaan
+${prefix}nasib
+${prefix}penyakit
+${prefix}tarot
+${prefix}fengshui
+${prefix}haribaik
+${prefix}harisangar
+${prefix}harisial
+${prefix}nagahari
+${prefix}arahrezeki
+${prefix}peruntungan
+${prefix}weton
+${prefix}karakter
+${prefix}keberuntungan
+${prefix}memancing
+${prefix}masasubur
+${prefix}zodiak 
+${prefix}shio [query]
 
 
 ╭─❒ 「 Others 」
